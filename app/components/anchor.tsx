484eacafd52b3390672972b78c560eac11e15c2b
Anthony Powell <anthonypowell@hey.com>
2022-01-19 20:41:03 -0500

    Improve external link ux

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -4,7 +4,7 @@ import { Link, LinkProps } from "remix";
 import type { HTMLProps } from "react";
 
 const anchorBaseStyles = `
-decoration-slate-600 hover:decoration-slate-200 focus:decoration-slate-200
+decoration-slate-600 active:decoration-slate-200 hover:decoration-slate-200 focus:decoration-slate-200
 text-blue-100 underline underline-offset-2 transition-colors outline-none
 motion-reduce:transition-none font-bold`;
 
@@ -12,7 +12,14 @@ export function ExternalAnchor({
   className,
   ...props
 }: HTMLProps<HTMLAnchorElement>) {
-  return <a className={clsx(className, anchorBaseStyles)} {...props} />;
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className={clsx(className, anchorBaseStyles)}
+      {...props}
+    />
+  );
 }
 
 export function Anchor({ className, ...props }: LinkProps) {
