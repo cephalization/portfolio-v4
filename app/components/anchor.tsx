d5d4782ceee78520873bf6f071cd223fa2565cd9
Anthony Powell <anthonypowell@hey.com>
2022-01-20 19:50:01 -0500

    Minor style tweaks

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -4,7 +4,7 @@ import { Link, LinkProps } from "remix";
 import type { HTMLProps } from "react";
 
 const anchorBaseStyles = `
-decoration-slate-600 active:decoration-slate-200 hover:decoration-slate-200 focus:decoration-slate-200
+decoration-sky-400 active:decoration-slate-200 hover:decoration-slate-200 focus:decoration-slate-200
 text-blue-100 underline underline-offset-2 transition-colors outline-none
 motion-reduce:transition-none font-bold`;
 
